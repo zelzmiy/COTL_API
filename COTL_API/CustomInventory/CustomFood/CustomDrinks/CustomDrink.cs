61549e1caf5001f4be6dc83230627cb060c691a4
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-12-02 18:26:33 -0600

    chore: seal overridden derived members in custom meals
    public override sealed NielMcBeal

--- COTL_API/CustomInventory/CustomFood/CustomDrinks/CustomDrink.cs
+++ COTL_API/CustomInventory/CustomFood/CustomDrinks/CustomDrink.cs
@@ -3,12 +3,12 @@ namespace COTL_API.CustomInventory;
 public abstract class CustomDrink : CustomFood
 {
     internal FollowerBrain.PleasureActions PleasureAction { get; set; }
-    public override InventoryItem.ITEM_TYPE ItemPickUpToImitate { get; } = InventoryItem.ITEM_TYPE.DRINK_GIN;
+    public sealed override InventoryItem.ITEM_TYPE ItemPickUpToImitate { get; } = InventoryItem.ITEM_TYPE.DRINK_GIN;
 
     /// <summary>
     ///     The amount of Sin gained by the follower. Range: 0-65
     /// </summary>
     public virtual int Pleasure { get; } = 0;
 
-    public override int FoodSatitation { get; } = 0;
+    public sealed override int FoodSatitation { get; } = 0;
 }
--- COTL_API/CustomInventory/CustomFood/CustomFood.cs
+++ COTL_API/CustomInventory/CustomFood/CustomFood.cs
@@ -28,6 +28,6 @@ public abstract class CustomFood : CustomInventoryItem
     ///     Item display offset when in inventory storage such as a follower kitchen or pub
     /// </summary>
     public virtual Vector3 ItemDisplayOffset { get; } = Vector3.zero;
-    public override bool IsFood => true;
+    public sealed override bool IsFood => true;
 
 }
